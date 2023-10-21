a68306604bc8f631c230a9775fd59080553662f0
Priyanshu Rajput <priyanshurajput822400@gmail.com>
2023-10-21 10:01:42 +0530

    contest 900

--- 900/3.cpp
+++ 900/3.cpp
@@ -2,31 +2,13 @@
 using namespace std;
 #define int long long int
 
-void solve()
-{
-    int n, k, x;
-    cin >> n >> k >> x;
-  int p=n-k;
-    int nSum = (n * (n + 1)) / (long long)2;
-    int kSum = (k * (k + 1)) / 2;
-    int pSum = nSum-(p*(p+1))/2;
-    if (x > nSum || x < kSum || x>pSum)
-    {
-        cout << "NO" << endl;
-    }
-    else
-        cout << "YES" << endl;
-    return;
-}
+
 
 int32_t main()
 {
-    int t = 1;
-    cin >> t;
-    while (t--)
-    {
-        solve();
-    }
+    int x = 5;
+    int y = ~x;
+    cout<<y<<endl;
 
     return 0;
 }
